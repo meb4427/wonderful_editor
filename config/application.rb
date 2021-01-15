dc94e6fe0bafa9ed3fa020aea40b5fbcbcf2017e
meb4427 <sadanori.maeba@gmail.com>
2021-01-15 19:58:45 +0900

    generator の実行時に，テスト環境が整うように application.rb を設定。

--- config/application.rb
+++ config/application.rb
@@ -31,11 +31,17 @@ module WonderfulEditor
 
     # Don't generate system test files.
     config.generators do |g|
+      g.template_engine false
       g.javascripts false
       g.stylesheets false
       g.helper false
-      g.template_engine false
-      g.test_framework :rspec
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
     end
+  config.api_only = true
   end
 end
