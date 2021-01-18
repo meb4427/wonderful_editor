c3db0e2ad2d41e53ab6e5feace0c37c67b0d9e45
meb4427 <sadanori.maeba@gmail.com>
2021-01-18 18:13:16 +0900

    重複アカウントが発生しないよう， validate を追記。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :account, presence: true, uniqueness: { case_sensitive: false }
   has_many :articles, dependent: :destroy
   has_many :article_likes, dependent: :destroy
   has_many :article_comments, dependent: :destroy
