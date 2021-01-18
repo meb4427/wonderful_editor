484c60e4e1ce5cbb5c993b4317d4cb704455928e
meb4427 <sadanori.maeba@gmail.com>
2021-01-18 17:43:33 +0900

    has_many の dependent_option の記載漏れの追記。

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :article_likes
-  has_many :article_comments
+  has_many :article_likes, dependent: :destroy
+  has_many :article_comments, dependent: :destroy
 end
